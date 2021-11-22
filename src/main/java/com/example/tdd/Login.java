0578b223354eada75f8f4e52e4f749843fc67f3d
Ludvig Anderbeck <ludviganderbeck@gmail.com>
2021-11-22 21:07:52 +0100

    1.2 inloggning

--- src/main/java/com/example/tdd/Login.java
+++ src/main/java/com/example/tdd/Login.java
@@ -1,13 +1,18 @@
 package com.example.tdd;
 
+import java.util.List;
+
 public class Login {
 
-    public boolean loginValidator(User user, String userName, String password){
-        if (user.getUserName().equals(userName)){
-            if (user.getPassword().equals(password)){
-                return true;
+    public boolean loginValidator(List<User> userList, String userName, String password){
+        for (User user : userList){
+            if (user.getUserName().equals(userName)){
+                if (user.getPassword().equals(password)){
+                    return true;
+                }
             }
         }
+
         return false;
     }
 }
--- src/test/java/com/example/tdd/TddProjectApplicationTests.java
+++ src/test/java/com/example/tdd/TddProjectApplicationTests.java
@@ -6,10 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class TddProjectApplicationTests {
-	UserFactory userFactory = new UserFactory();
-	User user = userFactory.createUser("Namn", "lösen");
+	List<User> userList = new ArrayList<>();
 
 	@Test
 	void contextLoads() {
@@ -18,14 +20,18 @@ class TddProjectApplicationTests {
 	@BeforeEach
 	@Test
 	void userFactory_test_success(){
-
+		UserFactory userFactory = new UserFactory();
+		User user = userFactory.createUser("anna", "losen");
+		User user1 = userFactory.createUser("berit", "123456");
+		User user2 = userFactory.createUser("kalle", "password");
+		userList = List.of(user,user1,user2);
 		assertNotNull(user);
 	}
 
 	@Test
 	void user_login_test_success(){
 		Login login = new Login();
-		Boolean success = login.loginValidator(user, "Namn", "lösen");
+		Boolean success = login.loginValidator(userList, "berit", "123456");
 
 		assertEquals(true, success);
 	}
